ff9267328d208af0e9537493f2994547da70a8ce
Dhiman Sarker Bappi <dhiman.sarker@selise.ch>
2022-12-19 15:56:49 +0600

    slider done

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,10 +1,21 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, Input, OnInit , AfterViewInit } from '@angular/core';
 import { Movie } from '../../models/movie';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.scss']
+  styleUrls: ['./slider.component.scss'],
+  animations: [
+    trigger('fade',[
+      state('void',style({
+        opacity:0
+      })),
+      transition('void => *' , [animate('1s')]),
+      transition('* => void' , [animate('500ms')]),
+
+    ])
+  ]
 })
 export class SliderComponent implements OnInit ,AfterViewInit{
   @Input() items:Movie[] = [];
