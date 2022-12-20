679d670ae3a2b41758d7e34badcd804c983e21ce
Dhiman Sarker <dhiman.sarker@selise.ch>
2022-12-20 20:44:04 +0600

    item

--- src/app/components/items-banner/items-banner.component.ts
+++ src/app/components/items-banner/items-banner.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Movie } from 'src/app/models/movie';
 
 @Component({
   selector: 'app-items-banner',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./items-banner.component.scss']
 })
 export class ItemsBannerComponent implements OnInit {
+  @Input() items:Movie[]=[];
 
   constructor() { }
 
--- src/app/components/movie-item/movie-item.component.ts
+++ src/app/components/movie-item/movie-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Movie } from 'src/app/models/movie';
 
 @Component({
   selector: 'app-movie-item',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-item.component.scss']
 })
 export class MovieItemComponent implements OnInit {
+  @Input() item:Movie | null = null;
 
   constructor() { }
 
